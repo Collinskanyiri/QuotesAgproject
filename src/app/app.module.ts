444cc5226d12316b102410d83b3a68fc3b2de823
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-08-22 19:48:48 +0300

    add app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { DatePipe } from './date.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { HighlightQouteDirective } from './highlight-qoute.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    QuoteDetailsComponent,
+    DatePipe,
+    QuoteFormComponent,
+    HighlightQouteDirective
   ],
   imports: [
     BrowserModule,
